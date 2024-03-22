021e6e2f987f76cf504a0301ccc3e614568da32c
Himanshu Gabani <himanshugabani28@gmail.com>
2024-03-22 16:45:41 +0530

    -

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors=require("cors");
 const app=express();
 connectDb();
 
-const port = 3000;//process.env.PORT || 5001;
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
